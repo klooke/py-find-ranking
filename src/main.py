d1c682f27ef81a2905cf071ee2b6ab772b50f3a0
Kevin Oliveira <55069829+klooke@users.noreply.github.com>
2024-04-29 12:22:19 -0300

    Challenge resolution

---
+++ src/main.py
@@ -0,0 +1,34 @@
+def get_player_ranking(vitorias):
+    if vitorias <= 10:
+        return "Ferro"
+    elif vitorias <= 20:
+        return "Bronze"
+    elif vitorias <= 50:
+        return "Prata"
+    elif vitorias <= 80:
+        return "Ouro"
+    elif vitorias <= 90:
+        return "Diamante"
+    elif vitorias <= 100:
+        return "Lendario"
+    else:
+        return "Imortal"
+
+
+print("\nBem vindo á calculadora de partidas rankeadas!\n")
+
+while True:
+    print("Digite número de vitórias e derrotas do Herói: (deixe vazio para sair)")
+
+    try:
+        vitorias = int(input("-Nº de vitórias: "))
+        derrotas = int(input("-Nº de derrotas: "))
+        saldoVitorias = vitorias - derrotas
+
+        nivel = get_player_ranking(vitorias)
+    except (ValueError, EOFError):
+        break
+
+    print(
+        f"\nO Herói tem de saldo de {saldoVitorias} está no nível de {nivel}"
+    )
